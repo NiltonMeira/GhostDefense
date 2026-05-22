d178761006818a8871c97319437723c5e18b5b05
Nilton Meira <niltonprudys@gmail.com>
2026-05-22 01:18:27 -0300

    Feat: first boss

--- enemy_spawner.gd
+++ enemy_spawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var enemy_scene: PackedScene
+@export var boss_scene: PackedScene
 
 @onready var paths = [
 	$"../Path2D",
@@ -14,38 +15,63 @@ var waves = [
 	{
 		"count": 2,
 		"speed": 40.0,
-		"spawn_delay": 1.5
-	},
+		"spawn_delay": 1.8
+	}, # tutorial
+
 	{
-		"count": 2,
+		"count": 3,
+		"speed": 45.0,
+		"spawn_delay": 1.6
+	},
+
+	{
+		"count": 5,
+		"speed": 50.0,
+		"spawn_delay": 1.4
+	},
+
+	{
+		"count": 6,
 		"speed": 55.0,
+		"spawn_delay": 1.3
+	},
+
+	{
+		"count": 8,
+		"speed": 60.0,
 		"spawn_delay": 1.2
 	},
+
 	{
-		"count": 4,
+		"count": 10,
 		"speed": 70.0,
 		"spawn_delay": 1.0
 	},
+
 	{
-		"count": 6,
+		"count": 12,
+		"speed": 80.0,
+		"spawn_delay": 0.9
+	},
+
+	{
+		"count": 14,
 		"speed": 90.0,
 		"spawn_delay": 0.8
 	},
+
 	{
-		"count": 8,
-		"speed": 90.0,
-		"spawn_delay": 0.8
+		"count": 16,
+		"speed": 100.0,
+		"spawn_delay": 0.7
 	},
+
 	{
-		"count": 10,
-		"speed": 90.0,
-		"spawn_delay": 0.8
-	},
-	{
-		"count": 5,
+		"count": 1,
 		"speed": 200.0,
-		"spawn_delay": 0.2
-	},
+		"spawn_delay": 0.2,
+		"boss": true
+	} 
 ]
 
 func _ready():
@@ -66,23 +92,29 @@ func start_waves():
 			
 		get_tree().current_scene.add_money(reward)
 
-		await get_tree().create_timer(2.0).timeout
+		await get_tree().create_timer(0).timeout
 
 	get_tree().current_scene.show_you_win()
 
 func spawn_wave(wave, wave_index):
+
 	for i in range(wave["count"]):
 
-		var enemy = enemy_scene.instantiate()
+		var enemy
+
+		if wave.has("boss") and wave["boss"]:
+			enemy = boss_scene.instantiate()
+		else:
+			enemy = enemy_scene.instantiate()
 
 		enemy.speed = wave["speed"]
 
-		var skeleton = enemy.get_node("Skeleton")
+		var body = enemy.get_child(0)
 
-		# aumenta 2 de vida por rodada
-		skeleton.health += wave_index * 2
+		body.health += wave_index * 5
 
 		var selected_path = paths.pick_random()
+
 		selected_path.add_child(enemy)
 
 		await get_tree().create_timer(
